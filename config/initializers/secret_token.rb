e8c43e36ccec0e24058296265a775d8d4cc555eb
Low-Wee Heng <alpha116@gmail.com>
2012-05-23 18:23:55 +0800

    add

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Citprov::Application.config.secret_token = '058d2bc446e62e5715a2e83305d92442df17a80677ae3644f34b9422d60d983286175bec467fc96781b0f7ef811d64b43aef3d153063cb2b91545ce514324690'
