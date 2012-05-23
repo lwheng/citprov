971a03e2c2fdcd3b54292d55bcf9e027d1907fd3
Low-Wee Heng <alpha116@gmail.com>
2012-05-24 01:11:54 +0800

    fixed navigation bar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,22 +1,47 @@
 class PagesController < ApplicationController
   def home
     @title = "Home"
+    @home = "active"
+    @citation = ""
+    @annotate = ""
+    @about = ""
+    @contact = ""
   end
 
   def citation
     @title = "Citation"
+    @home = ""
+    @citation = "active"
+    @annotate = ""
+    @about = ""
+    @contact = ""
   end
 
   def annotate
     @title = "Annotate"
+    @home = ""
+    @citation = ""
+    @annotate = "active"
+    @about = ""
+    @contact = ""
   end
 
   def about
     @title = "About"
+    @home = ""
+    @citation = ""
+    @annotate = ""
+    @about = "active"
+    @contact = ""
   end
 
   def contact
     @title = "Contact"
+    @home = ""
+    @citation = ""
+    @annotate = ""
+    @about = ""
+    @contact = "active"
   end
 
 end
