540348d3e303a96383aecb41a6158086bb618072
Low-Wee Heng <alpha116@gmail.com>
2012-06-05 23:24:11 +0800

    testing open-uri

--- app/models/cited_citees.rb
+++ app/models/cited_citees.rb
@@ -1,9 +1,16 @@
+require 'open-uri'
+	
 class CitedCitees < ActiveRecord::Base
 	attr_accessible :cited, :citees
 	def self.loaddata()
+    # This method should only be ran once
 		file = File.open("#{Rails.root}/app/assets/data/cited-citees.txt", "r")
 		while (line = file.gets)
 		  puts "#{line}"
 		end
+		
+		file1 = open("http://wing.comp.nus.edu.sg/~antho/A/A00/A00-3008-merge.xml", "r")
+		data = file1.read
+		puts data
 	end
 end
