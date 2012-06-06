9af28672e88e4d5437e87f2520b13ca064680122
Low-Wee Heng <alpha116@gmail.com>
2012-06-06 16:59:03 +0800

    trying

--- db/migrate/20120530112916_create_annotations.rb
+++ db/migrate/20120530112916_create_annotations.rb
@@ -1,10 +1,10 @@
 class CreateAnnotations < ActiveRecord::Migration
-  # def change
-  #   create_table :annotations do |t|
-  #     t.string :cite_key
-  #     t.text :user
-  # 
-  #     t.timestamps
-  #   end
-  # end
+  def up
+    create_table :annotations do |t|
+      t.string :cite_key
+      t.text :user
+  
+      t.timestamps
+    end
+  end
 end
