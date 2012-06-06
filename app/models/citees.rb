eabf9d0518725200bc4e0101ca36f09b3a72fa5c
Low-Wee Heng <alpha116@gmail.com>
2012-06-06 18:01:32 +0800

    ok deployed

--- app/models/citees.rb
+++ app/models/citees.rb
@@ -36,10 +36,6 @@ class Citees < ActiveRecord::Base
   def self.citing(citing)
     return "<h1>Hello World</h1>".html_safe
   end
-  
-  def self.testing()
-    return "Hello World"
-  end
 end
 # == Schema Information
 #
