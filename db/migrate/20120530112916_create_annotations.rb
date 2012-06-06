ca8ce96ae4f8a196ba9204fe700e212470267146
Low-Wee Heng <alpha116@gmail.com>
2012-06-06 16:56:02 +0800

    trying migration by commenting

--- db/migrate/20120530112916_create_annotations.rb
+++ db/migrate/20120530112916_create_annotations.rb
@@ -1,10 +1,10 @@
 class CreateAnnotations < ActiveRecord::Migration
-  def change
-    create_table :annotations do |t|
-      t.string :cite_key
-      t.text :user
-
-      t.timestamps
-    end
-  end
+  # def change
+  #   create_table :annotations do |t|
+  #     t.string :cite_key
+  #     t.text :user
+  # 
+  #     t.timestamps
+  #   end
+  # end
 end
