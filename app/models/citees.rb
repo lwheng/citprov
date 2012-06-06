cc4943ba2b4e9263d84cfd8e79669e738ba91e00
Low-Wee Heng <alpha116@gmail.com>
2012-06-06 17:27:58 +0800

    ok

--- app/models/citees.rb
+++ app/models/citees.rb
@@ -1,46 +1,41 @@
-require 'open-uri'
-require 'nokogiri'
+# require 'open-uri'
 
 class Citees < ActiveRecord::Base
   attr_accessible :cited, :citing
-  def self.loaddata()
-    # This method should only be ran once
-    # First remove all records in table
-    Citees.delete_all
-
-    # Now we add all records
-    file = File.open("#{Rails.root}/app/assets/data/cited-citees.txt", "r")
-    while (line = file.gets)
-      info = line.gsub("\n","").split("=")
-      key = info[0]
-      values = info[1]
-      newRecord = Citees.new
-      newRecord.cited = key
-      newRecord.citing = values
-      newRecord.save
-    end
-  end
   
-  def self.cited(cited)
-    # Fetch paper with id, cited
-    file = open("http://wing.comp.nus.edu.sg/~antho/#{cited[0]}/#{cited[0,3]}/#{cited}-pdfbox.txt","r")
-    data = ""
-    id = 1
-    while (line = file.gets)
-      data = "#{data}<div id=\"line#{id}\">#{line.gsub("\n","")}</div>#{"\n"}"
-      id += 1
-    end
-    return data.html_safe
-  end
-  
-  def self.citing(citing)
-    return "<h1>Hello World</h1>".html_safe
-  end
-  
-  def self.get_citing_sent(cited, citing)
-    citings = Citees.find_by_cited(cited).citing
-    puts citings
-  end
+  # def self.loaddata()
+  #   # This method should only be ran once
+  #   # First remove all records in table
+  #   Citees.delete_all
+  # 
+  #   # Now we add all records
+  #   file = File.open("#{Rails.root}/app/assets/data/cited-citees.txt", "r")
+  #   while (line = file.gets)
+  #     info = line.gsub("\n","").split("=")
+  #     key = info[0]
+  #     values = info[1]
+  #     newRecord = Citees.new
+  #     newRecord.cited = key
+  #     newRecord.citing = values
+  #     newRecord.save
+  #   end
+  # end
+  # 
+  # def self.cited(cited)
+  #   # Fetch paper with id, cited
+  #   file = open("http://wing.comp.nus.edu.sg/~antho/#{cited[0]}/#{cited[0,3]}/#{cited}-pdfbox.txt","r")
+  #   data = ""
+  #   id = 1
+  #   while (line = file.gets)
+  #     data = "#{data}<div id=\"line#{id}\">#{line.gsub("\n","")}</div>#{"\n"}"
+  #     id += 1
+  #   end
+  #   return data.html_safe
+  # end
+  # 
+  # def self.citing(citing)
+  #   return "<h1>Hello World</h1>".html_safe
+  # end
 end
 # == Schema Information
 #
