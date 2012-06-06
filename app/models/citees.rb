e3c5f9a55e59fffc6c63bca563399bd997ea1df0
Low-Wee Heng <alpha116@gmail.com>
2012-06-06 17:43:42 +0800

    almost

--- app/models/citees.rb
+++ app/models/citees.rb
@@ -1,41 +1,41 @@
-# require 'open-uri'
+require 'open-uri'
 
 class Citees < ActiveRecord::Base
   attr_accessible :cited, :citing
   
-  # def self.loaddata()
-  #   # This method should only be ran once
-  #   # First remove all records in table
-  #   Citees.delete_all
-  # 
-  #   # Now we add all records
-  #   file = File.open("#{Rails.root}/app/assets/data/cited-citees.txt", "r")
-  #   while (line = file.gets)
-  #     info = line.gsub("\n","").split("=")
-  #     key = info[0]
-  #     values = info[1]
-  #     newRecord = Citees.new
-  #     newRecord.cited = key
-  #     newRecord.citing = values
-  #     newRecord.save
-  #   end
-  # end
-  # 
-  # def self.cited(cited)
-  #   # Fetch paper with id, cited
-  #   file = open("http://wing.comp.nus.edu.sg/~antho/#{cited[0]}/#{cited[0,3]}/#{cited}-pdfbox.txt","r")
-  #   data = ""
-  #   id = 1
-  #   while (line = file.gets)
-  #     data = "#{data}<div id=\"line#{id}\">#{line.gsub("\n","")}</div>#{"\n"}"
-  #     id += 1
-  #   end
-  #   return data.html_safe
-  # end
-  # 
-  # def self.citing(citing)
-  #   return "<h1>Hello World</h1>".html_safe
-  # end
+  def self.loaddata()
+    # This method should only be ran once
+    # First remove all records in table
+    Citees.delete_all
+
+    # Now we add all records
+    file = File.open("#{Rails.root}/app/assets/data/cited-citees.txt", "r")
+    while (line = file.gets)
+      info = line.gsub("\n","").split("=")
+      key = info[0]
+      values = info[1]
+      newRecord = Citees.new
+      newRecord.cited = key
+      newRecord.citing = values
+      newRecord.save
+    end
+  end
+
+  def self.cited(cited)
+    # Fetch paper with id, cited
+    file = open("http://wing.comp.nus.edu.sg/~antho/#{cited[0]}/#{cited[0,3]}/#{cited}-pdfbox.txt","r")
+    data = ""
+    id = 1
+    while (line = file.gets)
+      data = "#{data}<div id=\"line#{id}\">#{line.gsub("\n","")}</div>#{"\n"}"
+      id += 1
+    end
+    return data.html_safe
+  end
+
+  def self.citing(citing)
+    return "<h1>Hello World</h1>".html_safe
+  end
 end
 # == Schema Information
 #
