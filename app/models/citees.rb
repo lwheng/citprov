24a43a75d33f04aa0c6a5eb3b87e8568f2ab56b9
Low-Wee Heng <alpha116@gmail.com>
2012-06-08 16:51:03 +0800

    switched to using parscit-section.xml files to display cited paper. added formatting too to make it more readable. added PDF link for cited paper

--- app/models/citees.rb
+++ app/models/citees.rb
@@ -5,15 +5,6 @@ include REXML
 class Citees < ActiveRecord::Base
   attr_accessible :cited, :citing
   
-  def self.testing()
-    puts "hello"
-    string = "<mydoc attr='ok'><first>test</first><second>test again</second></mydoc>"
-    doc = Document.new string
-    root = doc.root
-    puts root.elements["first"].text
-    return root.elements["first"].text
-  end
-  
   def self.loaddata()
     # This method should only be ran once
     # First remove all records in table
@@ -57,14 +48,155 @@ class Citees < ActiveRecord::Base
     # Fetch paper with id, cited
     cited = arg.split("==>")[1]
     
-    file = open("http://wing.comp.nus.edu.sg/~antho/#{cited[0]}/#{cited[0,3]}/#{cited}-pdfbox.txt","r")
-    data = ""
-    id = 1
-    while (line = file.gets)
-      data = "#{data}<div id=\"line#{id}\">#{line.gsub("\n","")}</div>#{"\n"}"
-      id += 1
+    file = open("http://wing.comp.nus.edu.sg/~antho/#{cited[0]}/#{cited[0,3]}/#{cited}-parscit-section.xml","r")
+    data = file.read
+    root = (Document.new data).root
+    content = root.elements["algorithm"].elements["variant"]
+    
+    # content is <variant> in *-parscit-section.xml
+    # Display content with better formatting
+    display = ""
+    content.elements.each do |tag|
+      case tag.name
+        
+      when "address"
+        address = tag.text
+        address_lines = address.split("\n")
+        address_lines.each do |v|
+          if v.length > 0
+            display = "#{display}<div><i>#{v}</i></div>"
+          end
+        end
+      when "affiliation"
+        affiliation = tag.text
+        affiliation_lines = affiliation.split("\n")
+        affiliation_lines.each do |v|
+          if v.length > 0
+            display = "#{display}<div><i>#{v}</i></div>"
+          end
+        end
+      when "author"
+        display = "#{display}<div><h4>#{tag.text}</h4></div>"
+      when "bodyText"
+        bodyText = tag.text
+        bodyText_lines = bodyText.split("\n")
+        bodyText_lines.each do |v|
+          if v.length > 0
+            display = "#{display}<div>#{v}</div>"
+          end
+        end
+      when "category"
+        category = tag.text
+        category_lines = category.split("\n")
+        category_lines.each do |v|
+          if v.length > 0
+            display = "#{display}<div>#{v}</div>"
+          end
+        end
+      when "construct"
+        construct = tag.text
+        construct_lines = construct.split("\n")
+        construct_lines.each do |v|
+          if v.length > 0
+            display = "#{display}<div>#{v}</div>"
+          end
+        end
+      when "copyright"
+        copyright = tag.text
+        copyright_lines = copyright.split("\n")
+        copyright_lines.each do |v|
+          if v.length > 0
+            display = "#{display}<div><i>#{v}</i></div>"
+          end
+        end
+      when "email"
+        display = "#{display}<div>#{tag.text}</div>"
+      when "equation"
+        equation = tag.text
+        equation_lines = equation.split("\n")
+        equation_lines.each do |v|
+          if v.length > 0
+            display = "#{display}<div><i>#{v}</i></div>"
+          end
+        end
+      when "figure"
+        figure = tag.text
+        figure_lines = figure.split("\n")
+        figure_lines.each do |v|
+          if v.length > 0
+            display = "#{display}<div><i>#{v}</i></div>"
+          end
+        end
+      when "figureCaption"
+        display = "#{display}<div><i><b>#{tag.text}</b></i></div>"
+      when "footnote"
+        footnote = tag.text
+        footnote_lines = footnote.split("\n")
+        footnote_lines.each do |v|
+          if v.length > 0
+            display = "#{display}<div><i><span class='muted'>#{v}</span</i></div>"
+          end
+        end
+      when "keyword"
+        display = "#{display}<div><b>#{tag.text}</b></div>"
+      when "listItem"
+        listItem = tag.text
+        listItem_lines = listItem.split("\n")
+        listItem_lines.each do |v|
+          if v.length > 0
+            display = "#{display}<div>#{v}</div>"
+          end
+        end
+      when "note"
+        display = "#{display}<div>#{tag.text}</div>"
+      when "page"
+        display = "#{display}<div><span class='muted'>#{tag.text}</span></div>"
+      when "reference"
+        reference = tag.text
+        reference_lines = reference.split("\n")
+        reference_lines.each do |v|
+          if v.length > 0
+            display = "#{display}<div><i>#{v}</i></div>"
+          end
+        end
+      when "sectionHeader"
+        display = "#{display}<h4>#{tag.text}</h4>"
+      when "subsectionHeader"
+        display = "#{display}<h4>#{tag.text}</h4>"
+      when "subsubsectionHeader"
+        display = "#{display}<h4>#{tag.text}</h4>"
+      when "table"
+        table = tag.text
+        table_lines = table.split("\n")
+        table_lines.each do |v|
+          if v.length > 0
+            display = "#{display}<div><i>#{v}</i></div>"
+          end
+        end
+      when "tableCaption"
+        display = "#{display}<div><i><b>#{tag.text}</b></i></div>"
+      when "title"
+        display = "#{display}<div><h3>#{tag.text}</h3></div>"
+      
+      end
     end
-    return data.html_safe
+    
+    return display.html_safe
+    
+    # file = open("http://wing.comp.nus.edu.sg/~antho/#{cited[0]}/#{cited[0,3]}/#{cited}-pdfbox.txt","r")
+    # data = ""
+    # id = 1
+    # while (line = file.gets)
+    #   data = "#{data}<div id=\"line#{id}\">#{line.gsub("\n","")}</div>#{"\n"}"
+    #   id += 1
+    # end
+    # return data.html_safe
+  end
+  
+  def self.cited_pdf_link(arg)
+    cited = arg.split("==>")[1]
+    return "http://wing.comp.nus.edu.sg/~antho/#{cited[0]}/#{cited[0,3]}/#{cited}.pdf"
+    
   end
 end
 # == Schema Information
