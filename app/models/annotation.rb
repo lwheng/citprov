f26ea498401f17b491e4273dd03cbf4fafea67ce
Low-Wee Heng <alpha116@gmail.com>
2012-07-11 15:48:08 +0800

    added context to model

--- app/models/annotation.rb
+++ app/models/annotation.rb
@@ -14,18 +14,32 @@ class Annotation < ActiveRecord::Base
     
     class << self
       def init()
-        file = File.open("#{Rails.root}/app/assets/data/annotations500.txt", "r")
-        while (cite_key = file.gets)
-          cite_key = cite_key.strip()
-          unless find_by_cite_key(cite_key)
-            newRecord = new(:cite_key => cite_key)
-            newRecord.annotations = {}
-            newRecord.annotation_count = 0
-            context = nil
-            newRecord.save
-          end
+        # file = File.open("#{Rails.root}/app/assets/data/annotations500.txt", "r")
+        # while (cite_key = file.gets)
+        #   cite_key = cite_key.strip()
+        #   unless find_by_cite_key(cite_key)
+        #     newRecord = new(:cite_key => cite_key)
+        #     newRecord.annotations = {}
+        #     newRecord.annotation_count = 0
+        #     newRecord.context = nil
+        #     newRecord.save
+        #   end
+        # end
+        # file.close()
+        
+        # To insert cite keys, and contexts
+        # Set annotations to blank, annotation counts to zero
+        file = File.open("#{Rails.root}/app/assets/data/context500.txt", "r")
+        while (line = file.gets)
+          info = line.split("!=")
+          cite_key = info[0]
+          context = info[1]
+          newRecord = new(:cite_key => cite_key)
+          newRecord.annotations = {}
+          newRecord.annotation_count = 0
+          newRecord.context = context
+          newRecord.save
         end
-        file.close()
       end
 
       def parse(user, annotation)
